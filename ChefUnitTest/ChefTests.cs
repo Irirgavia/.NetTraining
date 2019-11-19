2e53b2824581b8db9d21caaa72f5c2f056fa4282
Anastasiya <anastasia.kramushchenka@gmail.com>
2019-11-19 06:59:06 +0300

    fix: update unit tests

---
+++ ChefUnitTest/ChefTests.cs
@@ -0,0 +1,131 @@
+﻿namespace ChefUnitTest
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ChefLibrary;
+    using ChefLibrary.Products;
+    using ChefLibrary.Products.AnimalOriginProducts;
+    using ChefLibrary.Products.MixedProducts;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ChefTests
+    {
+        private ISalad testSalad;
+
+        private IProduct product1;
+
+        private IProduct product2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.product1 = new Egg(
+                "egg",
+                100,
+                new Caloricity(12.7, 10.9, 0.7),
+                "chicken",
+                Conditions.Boil);
+
+            this.product2 = new Sauce(
+                "mayonnaise",
+                50,
+                new Caloricity(0.0, 99.8, 0.0),
+                SauceСonsistency.Thick);
+
+            this.testSalad = new Salad("salad", new List<IProduct>() { this.product1, this.product2 });
+        }
+
+        [TestMethod]
+        public void GetSaladWeightTest()
+        {
+            double expectedWeight = 150;
+            Assert.AreEqual(
+                expectedWeight,
+                Chef.GetSaladWeight(this.testSalad));
+        }
+
+        [TestMethod]
+        public void GetSaladCaloriesTest()
+        {
+            double expectedWeight = 320.8443;
+            Assert.AreEqual(
+                expectedWeight,
+                Chef.GetSaladCalories(this.testSalad));
+        }
+
+        [TestMethod]
+        public void FindProductsByNameTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.FindProductsByName(this.testSalad, this.product1.Name).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void FindProductByPropertyTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.FindProductByProperty(this.testSalad.Products, x => x.Name == this.product1.Name).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void FindProductsByWeightTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.FindProductsByWeight(this.testSalad, this.product1.Weight).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void FindProductsByCaloriesTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.FindProductsByCalories(this.testSalad, this.product1.GetProductCalories(), this.product1.GetProductCalories()).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void SortProductsByPropertyTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product2, this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.SortProductsByProperty(this.testSalad.Products, x => x.Weight).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void SortSaladByNameTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product2, this.product2 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.SortSaladByName(this.testSalad).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void SortSaladByWeightTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product2, this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.SortSaladByWeight(this.testSalad).ToList().ToString());
+        }
+
+        [TestMethod]
+        public void SortSaladByCaloriesTest()
+        {
+            var expectedProduct = new List<IProduct>() { this.product2, this.product1 };
+            Assert.AreEqual(
+                expectedProduct.ToString(),
+                Chef.SortSaladByCalories(this.testSalad).ToList().ToString());
+        }
+    }
+}
--- ChefUnitTest/SaladTests.cs
+++ ChefUnitTest/SaladTests.cs
@@ -1,99 +1,56 @@
-﻿using ChefLibrary;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using ChefLibrary.Products;
-using ChefLibrary.Products.Egg;
-using ChefLibrary.Products.Vegetable;
-using ChefLibrary.Products.Cereal;
-using static ChefLibrary.Products.Vegetable.VegetableTypes;
-using static ChefLibrary.Products.Cereal.CerealTypes;
-
-namespace ChefLibrary.Tests
+﻿namespace ChefUnitTest
 {
-    [TestClass()]
+    using System.Collections.Generic;
+
+    using ChefLibrary;
+    using ChefLibrary.Products;
+    using ChefLibrary.Products.AnimalOriginProducts;
+    using ChefLibrary.Products.MixedProducts;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
     public sealed class SaladTests
     {
-        Salad testSalad;
+        private ISalad testSalad;
 
-        Product product1;
-        Product product2;
-        Product product3;
-        Product product4;
+        private IProduct product1;
 
+        private IProduct product2;
 
-        [TestInitialize()]
+        [TestInitialize]
         public void Initialize()
         {
-            product1 = new Egg("chikenEgg", 100, new Caloricity(12.7, 10.9, 0.7), "chicken", "boiled");
-            product2 = new Cucumber("cucumber", 200, new Caloricity(1.8, 1.1, 2.7), "raw", CucumberType.CucumberGermanShirt);
-            product3 = new Rice("baget", 100, new Caloricity(7.5, 2.9, 51.4), "milk", RiceType.RoundGrainRice);
-            product4 = new Sauce("oil", 50, new Caloricity(0.0, 99.8, 0.0), "oil");
+            this.product1 = new Egg(
+                "egg",
+                100,
+                new Caloricity(12.7, 10.9, 0.7),
+                "chicken",
+                Conditions.Boil);
 
-            testSalad = new Salad("testSalad", new List<Product>() { product1, product2, product3, product4});
+            this.product2 = new Sauce(
+                "mayonnaise",
+                50,
+                new Caloricity(0.0, 99.8, 0.0),
+                SauceСonsistency.Thick);
+
+            this.testSalad = new Salad("salad", new List<IProduct>() { this.product1 });
         }
 
-        [TestMethod()]
-        public void GetWeightTest()
+        [TestMethod]
+        public void AddProductTest()
         {
-            var expectedWeigth = 450.0;
-            Assert.AreEqual(expectedWeigth, testSalad.GetWeight());
+            this.testSalad.AddProduct(this.product2);
+            var expectedSalad = new Salad("salad", new List<IProduct>() { this.product1, this.product2 });
+            Assert.AreEqual(expectedSalad.ToString(), this.testSalad.ToString());
         }
 
-        [TestMethod()]
-        public void GetCaloriesTest()
+        [TestMethod]
+        public void RemoveProductTest()
         {
-            /*
-            1 * (9 * 10.9 + 4 * (12.7 + 0.7)) = 151.7
-            2 * (9 * 1.1 + 4 * (1.8 + 2.7)) = 55.8
-            1 * (9 * 2.9 + 4 * (7.5 + 51.4)) = 261.7
-            0,5 * (9 * 99.8 + 4 * (0.0 + 0.0)) = 449.1
-
-            151.7 + 55.8 + 261.7 + 449.1 = 918.3
-            */
-
-            var expectedCalories = 918.3;
-            Assert.AreEqual(expectedCalories, testSalad.GetCalories());
-        }
-
-        [TestMethod()]
-        public void FindProductsByNameTest()
-        {
-            var expectedProducts = new List<Product>() { product2 };
-            Assert.AreEqual(expectedProducts.ToString(), testSalad.FindProductsByName("salad").ToString());
-        }
-        [TestMethod()]
-        public void FindProductsByWeigthTest()
-        {
-            var expectedProducts = new List<Product>() { product1, product3 };
-            Assert.AreEqual(expectedProducts.ToString(), testSalad.FindProductsByWeigth(100).ToString());
-        }
-
-        [TestMethod()]
-        public void FindProductsByCaloriesTest()
-        {
-            var expectedProducts = new List<Product>() { product1, product3 };
-            Assert.AreEqual(expectedProducts.ToString(), testSalad.FindProductsByCalories(150.0, 340.6).ToString());
-        }
-
-        [TestMethod()]
-        public void SortByNameTest()
-        {
-            var expectedProducts = new List<Product>() { product3, product1, product4, product2 };
-            Assert.AreEqual(expectedProducts.ToString(), testSalad.SortByName().ToString());
-        }
-
-        [TestMethod()]
-        public void SortByWeigthTest()
-        {
-            var expectedProducts = new List<Product>() { product4, product1, product3, product2 };
-            Assert.AreEqual(expectedProducts.ToString(), testSalad.SortByWeigth().ToString());
-        }
-
-        [TestMethod()]
-        public void SortByCaloriesTest()
-        {
-            var expectedProducts = new List<Product>() { product4, product1, product3, product2 };
-            Assert.AreEqual(expectedProducts.ToString(), testSalad.SortByCalories().ToString());
+            this.testSalad.RemoveProduct(this.product1);
+            var expectedSalad = new Salad("salad", new List<IProduct>());
+            Assert.AreEqual(expectedSalad.ToString(), this.testSalad.ToString());
         }
     }
 }
