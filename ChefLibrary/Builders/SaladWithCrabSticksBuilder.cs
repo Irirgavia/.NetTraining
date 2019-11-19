83dc3d916df700e2c028199d3e47e441c498d78c
Anastasiya <anastasia.kramushchenka@gmail.com>
2019-11-19 03:06:18 +0300

    feat: add new salad builder

---
+++ ChefLibrary/Builders/SaladWithCrabSticksBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace ChefLibrary.Builders
+{
+    using System.Security;
+
+    using ChefLibrary.Products;
+    using ChefLibrary.Products.AnimalOriginProducts;
+
+    public class SaladWithCrabSticksBuilder : SaladBuilder
+    {
+        public SaladWithCrabSticksBuilder()
+            : base("Salad with crab sticks")
+        {
+            this.Reset(this.Salad.Name);
+        }
+
+        public override void BuildSalad()
+        {
+            this.AddProduct(this.GetPeelOffBoilChopEgg(165));
+            this.AddProduct(this.GetRawCucumber(100));
+            this.AddProduct(this.GetChopCrabSticks(200));
+            this.AddProduct(this.GetMayonnaise(50));
+        }
+
+        public Meat GetChopCrabSticks(double weight)
+        {
+            var crabSticks = new Meat(
+                "Crab Sticks",
+                weight,
+                new Caloricity(12, 2, 20),
+                "crab",
+                Conditions.Raw);
+
+            this.ChopProduct(crabSticks);
+            return crabSticks;
+        }
+    }
+}
