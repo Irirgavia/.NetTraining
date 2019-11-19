11efc894169fc017bd46862dbea08a6506218294
Anastasiya <anastasia.kramushchenka@gmail.com>
2019-11-19 03:08:29 +0300

    refactor: change based on a new structure

--- Chef/Program.cs
+++ Chef/Program.cs
@@ -1,55 +1,38 @@
-﻿using ChefLibrary;
-using ChefLibrary.Products;
-using ChefLibrary.Products.Cereal;
-using ChefLibrary.Products.Egg;
-using ChefLibrary.Products.Vegetable;
-using System;
-using System.Collections.Generic;
-using static ChefLibrary.Products.Cereal.CerealTypes;
-using static ChefLibrary.Products.Vegetable.VegetableTypes;
-
-namespace Chef
+﻿namespace Chef
 {
-    class Program
+    using System;
+
+    using ChefLibrary;
+    using ChefLibrary.Builders;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             try
             {
-                Product product1 = new Egg("chikenEgg", 100, new Caloricity(12.7, 10.9, 0.7), "chicken", "boiled");
-                Product product2 = new Cucumber("cucumber", 200, new Caloricity(1.8, 1.1, 2.7), "raw", CucumberType.CucumberGermanShirt);
-                Product product3 = new Rice("baget", 100, new Caloricity(7.5, 2.9, 51.4), "milk", RiceType.RoundGrainRice);
-                Product product4 = new Sauce("oil", 50, new Caloricity(0.0, 99.8, 0.0), "oil");
+                var chef = new Chef(new OlivierSaladBuilder());
+                var salad = chef.BuildSalad();
 
-                Salad salad = new Salad("testSalad", new List<Product>() { product1, product2, product3, product4});
+                Console.WriteLine(salad.ToString());
 
-                Console.WriteLine("Salad components:");
-                foreach(var product in salad.Products)
+                Console.WriteLine($"Salad weight: {chef.GetSaladWeight(salad)}g");
+                Console.WriteLine($"Salad caloricity: {chef.GetSaladCalories(salad)} kcal \n");
+
+                Console.WriteLine("Sort product by calories:");
+                foreach (var product in chef.SortSaladByCalories(salad))
                 {
-                    Console.WriteLine(product.ToString());
+                    Console.WriteLine(product);
                 }
 
-                Console.WriteLine("---");
+                double beginValue = 400;
+                double endValue = 1500;
 
-                Console.WriteLine($"Salad weigth: {salad.GetWeight()}");
-                Console.WriteLine($"Salad caloricity: {salad.GetCalories()}");
-
-                Console.WriteLine("---");
-
-                Console.WriteLine("Sort salad products by name:");
-                foreach (var product in salad.SortByName())
+                Console.WriteLine($"\nFind product with {beginValue}-{endValue} caloricity:");
+                foreach (var product in chef.FindProductsByCalories(salad, beginValue, endValue))
                 {
-                    Console.WriteLine(product.ToString());
+                    Console.WriteLine(product);
                 }
-
-                Console.WriteLine("---");
-
-                Console.WriteLine("Salad products with 150.0 - 320.0 caloricity:");
-                foreach (var product in salad.FindProductsByCalories(150.0, 320.0))
-                {
-                    Console.WriteLine(product.ToString());
-                }
-
             }
             catch (Exception ex)
             {
