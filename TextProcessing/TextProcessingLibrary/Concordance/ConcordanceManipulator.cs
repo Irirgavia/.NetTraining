25465cae385f4b44a9aae1fa2546a1418d8914a0
Anastasiya <anastasia.kramushchenka@gmail.com>
2019-11-29 16:50:16 +0300

    feat: Concordance

---
+++ TextProcessing/TextProcessingLibrary/Concordance/ConcordanceManipulator.cs
@@ -0,0 +1,78 @@
+﻿namespace TextProcessingLibrary.Concordance
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using TextProcessingLibrary.TextItems;
+    using TextProcessingLibrary.TextItems.SentenceItems;
+
+    public class ConcordanceManipulator
+    {
+        private List<ConcordanceWord> ConcordanceWords { get; set; }
+
+        public List<ConcordanceWord> CreateDictionary(Text text)
+        {
+            this.ConcordanceWords = new List<ConcordanceWord>();
+            var usedWords = new List<string>();
+
+            foreach (var paragraph in text.Paragraphs)
+            {
+                foreach (var sentence in paragraph.Sentences)
+                {
+                    foreach (var sentenceItem in sentence.SentenceItems)
+                    {
+                        if (sentenceItem.GetType() != typeof(Word))
+                        {
+                            continue;
+                        }
+
+                        var word = (Word)sentenceItem;
+                        var concordanceWord = new ConcordanceWord(word.ToString(), new List<int>() { word.LocationLine });
+
+                        var wordIndex = usedWords.IndexOf(concordanceWord.Word);
+
+                        if (wordIndex != -1)
+                        {
+                            if (this.ConcordanceWords[wordIndex].LocationLines.Exists(x => x > 1))
+                            {
+                                this.ConcordanceWords[wordIndex].EntriesCounts++;
+                                continue;
+                            }
+
+                            this.ConcordanceWords[wordIndex].LocationLines.Add(word.LocationLine);
+                        }
+                        else
+                        {
+                            this.ConcordanceWords.Add(concordanceWord);
+                            usedWords.Add(concordanceWord.Word);
+                        }
+                    }
+                }
+            }
+
+            return this.ConcordanceWords;
+        }
+
+        public List<ConcordanceWord> SortConcordanceWords()
+        {
+            return (from concordanceWord in this.ConcordanceWords
+                   orderby concordanceWord.Word
+                   select concordanceWord).ToList();
+        }
+
+        public override string ToString()
+        {
+            IEnumerable<char> alphabetLetters = new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            // TODO: division into groups
+            var stringBuilder = new StringBuilder();
+
+            foreach (var word in this.ConcordanceWords)
+            {
+                stringBuilder.Append($"{word}\n");
+            }
+            return base.ToString();
+        }
+    }
+}
---
+++ TextProcessing/TextProcessingLibrary/Concordance/ConcordanceWord.cs
@@ -0,0 +1,39 @@
+﻿namespace TextProcessingLibrary.Concordance
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ConcordanceWord
+    {
+        public ConcordanceWord(string word)
+        {
+            this.Word = word;
+            this.LocationLines = new List<int>();
+        }
+
+        public ConcordanceWord(string word, List<int> locationLines)
+        {
+            this.Word = word;
+            this.LocationLines = locationLines;
+        }
+
+        public string Word { get; private set; }
+
+        public List<int> LocationLines { get; private set; }
+
+        public int EntriesCounts { get; set; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder(this.Word);
+            stringBuilder.Append($"{this.EntriesCounts}:");
+            foreach (var locationLine in this.LocationLines)
+            {
+                stringBuilder.Append($" {locationLine},");
+            }
+
+            return stringBuilder.ToString().TrimEnd(',');
+        }
+    }
+}
