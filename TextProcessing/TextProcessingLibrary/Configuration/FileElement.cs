9f22105a537063b2b9b99f1927fa1cc214576529
Anastasiya <anastasia.kramushchenka@gmail.com>
2019-12-02 08:50:09 +0300

    feat: class for working with text

---
+++ TextProcessing/TextProcessingLibrary/Configuration/FileElement.cs
@@ -0,0 +1,21 @@
+﻿namespace TextProcessingLibrary.Configuration
+{
+    using System.Configuration;
+
+    public class FileElement : ConfigurationElement
+    {
+        [ConfigurationProperty("fileType", DefaultValue = "", IsKey = true, IsRequired = true)]
+        public string FileType
+        {
+            get => (string)base["fileType"];
+            set => base["fileType"] = value;
+        }
+
+        [ConfigurationProperty("filePath", DefaultValue = "", IsKey = false, IsRequired = false)]
+        public string FilePath
+        {
+            get => (string)base["filePath"];
+            set => base["filePath"] = value;
+        }
+    }
+}
---
+++ TextProcessing/TextProcessingLibrary/TextManipulator.cs
@@ -0,0 +1,61 @@
+﻿namespace TextProcessingLibrary
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    using TextProcessingLibrary.Parsers;
+    using TextProcessingLibrary.TextItems;
+    using TextProcessingLibrary.TextItems.SentenceItems;
+
+    public static class TextManipulator
+    {
+        public static IEnumerable<ISentence> GetSortedSentencesByWordsCount(IText text)
+        {
+            return from paragraph in text.Paragraphs
+                   from sentence in paragraph.Sentences
+                   orderby sentence.SentenceItems.Count
+                   select sentence;
+        }
+
+        public static IEnumerable<ISentenceItem> GetWordsFromInterrogativeSentences(IText text, int wordLength)
+        {
+            var result = from paragraph in text.Paragraphs
+                         from sentence in paragraph.Sentences
+                         where sentence.GetSentenceType() == SentenceType.Interrogative
+                         from word in sentence.SentenceItems
+                         where word.Symbols.Count == wordLength
+                         select word;
+
+            return result.Distinct();
+        }
+
+        public static IEnumerable<IParagraph> GetParagraphsWithoutWordsWithSymbolOnIndex(IText text, int wordLength, int index, char symbol)
+        {
+            return from paragraph in text.Paragraphs
+                   from sentence in paragraph.Sentences
+                   from word in sentence.SentenceItems 
+                   where word.Symbols.Count == wordLength && word.Symbols[index] != symbol
+                   select paragraph;
+        }
+
+        public static ISentence ReplaceWordsWithSubstring(ISentence sentence, int wordLength, string substring)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var sentenceItem in sentence.SentenceItems)
+            {
+                if (sentenceItem.Symbols.Count != wordLength)
+                {
+                    stringBuilder.Append(sentenceItem.Symbols.ToString());
+                    continue;
+                }
+
+                stringBuilder.Append(substring);
+            }
+
+            var parser = new Parser();
+            return parser.CreateSentence(stringBuilder.ToString(), 1);
+        }
+    }
+}
