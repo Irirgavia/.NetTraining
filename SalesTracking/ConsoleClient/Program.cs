d7f449d02996c22de5401adb0d0121d9964b42a8
Anastasiya <anastasia.kramushchenka@gmail.com>
2020-01-14 08:55:45 +0300

    feat: add console client

---
+++ SalesTracking/ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleClient
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using BLL;
+    using BLL.Configuration;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var manager = new Configuгator();
+            var folders = manager.Configure();
+
+            FolderTrackingWatcher watcher = new FolderTrackingWatcher(
+                folders.InitialFolder,
+                folders.ProcessedFolder,
+                folders.FaultedFolder);
+
+            Task task = Task.Factory.StartNew(() => watcher.Start());
+
+            Console.ReadKey();
+            watcher.Dispose();
+        }
+    }
+}
